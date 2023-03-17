7c8611f33834f31559a8bbdc5ac4f3c8c5bb06fd
jennifermcneil <mcn21005@byui.edu>
2023-03-17 12:12:07 -0700

    took off one of the repetative activities

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,15 +1,14 @@
 using System;
 
-class Program
+public class Program
 {
     static void Main(string[] args)
     {
         List<Activity> activities = new List<Activity>();
 
-        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 3.0));
-        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 4.8));
-        activities.Add(new Cycling(new DateTime(2022, 11, 3), 60, 15.0));
-        activities.Add(new Swimming(new DateTime(2022, 11, 3), 45, 20));
+        activities.Add(new RunningActivity(new DateTime(2022, 11, 3), 30, 3.0));
+        activities.Add(new CyclingActivity(new DateTime(2022, 11, 3), 60, 15.0));
+        activities.Add(new SwimmingActivity(new DateTime(2022, 11, 3), 45, 20));
 
         foreach (Activity activity in activities)
         {
